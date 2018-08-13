115485290a023540a1cddab5e113ae5db4f842da
capedcrusader23 <37747717+capedcrusader23@users.noreply.github.com>
2018-08-13 08:41:38 -0700

    Update route.js

--- route/route.js
+++ route/route.js
@@ -3,25 +3,40 @@ const router=express.Router();
 const ifsc=require('./file');
 const body=require('body-parser');
 var url=body.urlencoded({extended:false});
+var https=require('https');
 router.get('/',function(req,res){
   res.render("home");
 })
 
-router.post('/check',url,function(req,res) {
-    var k = ifsc.fetchDetails(req.body.ifsc);
-    if (k ==undefined) {
-        res.redirect('/error');
-    }
-    else {
+router.post('/check',url,function(req,response) {
+ var k='https://ifsc.razorpay.com/'+req.body.ifsc;
 
-        res.render('fnd',{info:k});
-    }
+ 
+    https.get(k, function(res){
+        var data='';
+
+        res.on('data', function(chunk){
+            data += chunk;
+        });
+
+
+        res.on('end', function(){
+            if(data.match('Not Found')){
+                response.redirect('/error');
+            }
+            else {
+                var result = JSON.parse(data);
+                response.render('fnd',{info:result});
+            }
+        });
+    }).on('error', function(e){
+        console.log(e);
+    });
 });
+
 
 router.get("/error",url,function(req,res){
     res.render('error');
 });
-
-
 
 module.exports=router;
